32fa213dc0fb5e6daccecdd5cdd40f2fa86b5ce5
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-08 17:38:25 -0400

    nav styling changes and initial footer concept

---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>&copy; 2021 Dakotah Godfrey</small>
+      <ul>
+        <li>
+          <a
+            aria-label="linked in profile"
+            className="social-link"
+            href="https://www.linkedin.com/in/dakotah-godfrey-2186091ab/"
+          >
+            <i className="fab fa-2x fa-linkedin-in"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            aria-label="twitter profile"
+            className="social-link"
+            href="https://www.twitter.com/dakotah_dev"
+          >
+            <i className="fab fa-2x fa-github-alt"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            aria-label="github profile"
+            className="social-link"
+            href="https://www.github.com/DakotahGodfrey"
+          >
+            <i className="fab fa-2x fa-twitter"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
