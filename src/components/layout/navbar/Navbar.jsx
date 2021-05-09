c00fc23a3f0ea8a3313de62fa5541d05bd18bc65
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-09 19:38:07 -0400

    inital about structure and copy

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -17,14 +17,15 @@ const Navbar = ({ positionFixed }) => {
             Home
           </Link>
         </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+
         <li>
           <Link to="/projects">Projects</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
         </li>
         <li>
           <button>
--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,7 +1,90 @@
 import React from "react";
-
+import Navbar from "../../layout/navbar/Navbar";
+import Footer from "../../layout/footer/Footer";
+import { Link } from "react-router-dom";
 const About = () => {
-  return <div></div>;
+  return (
+    <main className="main-about">
+      <Navbar />
+      <section className="about-section">
+        <article className="bio">
+          <h1 className="bio-lead">About Dakotah.</h1>
+          <h3>Hey there!</h3>
+          <p className="bio">
+            I'm Dakotah Godfrey, I'm a young community-taught web developer from
+            Canada. I'm new to the web development space, but it's quickly
+            become a passion. I've been focusing on building the foundational
+            skills needed to secure myself a job as a Frontend developer. Lately
+            I've been trying my hand at building accessible and beautiful web
+            apps with React and Redux.
+          </p>
+          <p className="bio">
+            Prior to the Covid-19 epidemic I was working in the hospitality
+            industry in Toronto. As work started to dry up, I began looking for
+            a change. So I dipped my toes into the coding world. Through
+            companies like{" "}
+            <a
+              className="shoutout"
+              href="https://www.codecademy.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Codecademy
+            </a>{" "}
+            and a plethora of online tutorials from amazing creators like{" "}
+            <a
+              className="shoutout"
+              href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Brad Traversy
+            </a>
+            ,{" "}
+            <a
+              href="https://jhey.dev/"
+              className="shoutout"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Jhey Dev
+            </a>
+            ,{" "}
+            <a
+              href="https://www.youtube.com/channel/UChoskCVZiIDTKxusGPPhLOg"
+              rel="noreferrer"
+              className="shoutout"
+              target="_blank"
+            >
+              Self Teach Me
+            </a>
+            , and others I grew to really enjoy the web development space and
+            community overall.
+          </p>
+          <p className="bio">
+            My Long term goal is to help make the tech community even more
+            accessible. I had always thought that a Computer Science or other{" "}
+            degree in{" "}
+            <abbr title="Science Tech Engineering and Math">STEM</abbr> was a
+            pre-requisite for getting into development, and I've never been so
+            glad to be wrong. If I can reach a spot where I can give back even a
+            little, of what I've gained from the dev community and help people
+            grow in this space. I'll able be to chalk this journey up as a
+            sucesss.{" "}
+          </p>
+          <p className="bio">
+            At the moment I'm looking to start an internship or a junior
+            developer, to continue to grow my skillset and gain experience.
+          </p>
+          <p className="bio">
+            Anyways that's me, if you'd like to get in touch please feel free to
+            reach out. <Link to="/contact">Contact.</Link>
+          </p>
+        </article>
+      </section>
+      <Footer />
+    </main>
+  );
 };
 
 export default About;
