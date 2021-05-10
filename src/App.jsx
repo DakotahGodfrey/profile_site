0f40f6661e35a7349a1a2d886e4bb58010fd786b
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-10 16:23:10 -0400

    cleared warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/layout/navbar/Navbar";
 import Home from "./components/pages/home/Home";
 import About from "./components/pages/about/About";
 import Contact from "./components/pages/contact/Contact";
 import Projects from "./components/pages/projects/Projects";
 import "./assets/scss/index.scss";
-import Footer from "./components/layout/footer/Footer";
 const App = () => {
   return (
     <div className="App">
