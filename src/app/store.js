86e3f890f35cd6fec5e04305b44f812708d909b2
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-11 10:19:28 -0400

    used redux-persist to persist dark mode.

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pagesSliceReducer from "../components/pages/pagesSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistPagesReducer = persistReducer(persistConfig, pagesSliceReducer);
 const store = configureStore({
   reducer: {
-    pagesSlice: pagesSliceReducer,
+    pagesSlice: persistPagesReducer,
   },
   initialState: "",
 });
 
+export let persistor = persistStore(store);
 export default store;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./app/store.js";
-
+import store, { persistor } from "./app/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
