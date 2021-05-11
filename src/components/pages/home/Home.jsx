4e8120461b2ce6821436a859488cbf754f35778b
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-10 21:24:11 -0400

    Home page copy, darkmode animations

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -16,19 +16,16 @@ const Home = () => {
       <Navbar />
       <section className="home-section">
         <article className="bio">
-          <h1 className="bio-lead">
+          <h1 className="bio-lead">Welcome,</h1>
+          <h3>
             <span>👋🏻</span>
-            Hey There, I'm Dakotah!
-          </h1>
+            Hey There, I'm Dakotah!{" "}
+          </h3>
           <p className="bio">
             I'm a self-taught Front-End developer from Toronto. I'm passionate
-            about learning new technologies. I love building accessible, user
-            friendly websites and web apps. Lately I've been working with React
-            and Redux and static hosting through providers like Firebase and
-            Netlify. I've also been taking any web development courses, I can
-            get my hands on. I'm interested in learning to be more of a
-            full-stack dev. Mostly so that I can have more freedom to digitally
-            express myself.
+            about accessible & responsive design. Lately I've been working on
+            desigining interactive web apps using React and Redux. I'm currently
+            open to new opportunities.
           </p>
         </article>
         <div className="call-to-action-links">
